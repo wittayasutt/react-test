d4ad666f3f358ca6b8e48adef8dd26b7c27e01dc
Ratthapon <wittayasutt@gmail.com>
2017-03-02 01:44:35 +0700

    update middleware for localhost

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ let app = express()
 const PORT = process.env.PORT || 3000
 
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next()
-  } else {
-    console.log('req.hostname', req.hostname)
-    console.log('req.url', req.url)
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url)
+  } else {
+    next()
   }
 })
 
