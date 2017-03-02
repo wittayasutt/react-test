0d764eb441ca9d1eda8fa658f45752ef06274c01
Ratthapon <wittayasutt@gmail.com>
2017-03-03 00:43:22 +0700

    style about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,23 @@ const React = require('react')
 
 let About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
